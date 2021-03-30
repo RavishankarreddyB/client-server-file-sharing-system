815ee7676105811d06a78f1e0c93e03948d38dfa
Ravi Shankar Reddy Bommana <ravishankarreddy.bommana@citrix.com>
2021-03-30 20:37:16 +0530

    Multi-client support

--- server.c
+++ server.c
@@ -2,12 +2,15 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <pthread.h> //for threads
 
 #include "socket.h"
 #include "header.h"
 
 #define PORT 8080
 #define BUFFERLENGTHFORLISTENING 4
+
+void *func(void *socketId);
 
 int main() {
 	int sockid = createSocket(4, 0); // 4 - IPv4, 0 - TCP. These are just internally used.
@@ -24,25 +27,46 @@ int main() {
 //	int newSocketId = acceptSocketReq(sockid, addr);
 	
 	int addrSize = sizeof(addr);
-        int newSocketId = accept(sockid, (struct sockaddr *) &addr, (socklen_t *)&addrSize);
-	if(newSocketId == -1)
-                printf("some problem connecting to client\n");
-        else
-                printf("connected to client and new socket id is %d\n", newSocketId);
+        int newSocketId, *newSock=(int*)malloc(sizeof(int));
+	while( ( newSocketId = accept(sockid, (struct sockaddr *) &addr, (socklen_t *)&addrSize) ) )
+	{
+		if(newSocketId == -1)
+                	printf("some problem connecting to client\n");
+        	else
+                	printf("connected to client and new socket id is %d\n", newSocketId);
 	
-	char *msg = "hello from server", buff[1024]={0};
-	send(newSocketId, msg, strlen(msg), 0);
+		char *msg = "hello from server";
+		send(newSocketId, msg, strlen(msg), 0);
+		
+		//thread related functions
+		pthread_t threadId;
+		*newSock=newSocketId;
+		
+		if( pthread_create(&threadId, NULL, func, (void *)newSock) < 0 )
+		{
+			printf("couldn't create thread\n");
+			exit(0);	
+		}
+	}	
+	
+	closeSocket(sockid);
+	return 0;
+}
+
+void *func(void *socketId) 
+{
+	char buff[1024]={0};
+	int newSocketId = *(int *)socketId;
 
 	recv(newSocketId, buff, 1024, 0);
 	printf("received this message from client:- '%s'\n", buff);
-
+	
 	if(checkFileExistence(buff) == 1) {
 		FILE *fp = fopen(buff, "r");
 		printf("sending the contents of file %s\n", buff);
 		bzero(buff, 1024);
 		readFromFileAndSendData(fp, buff, newSocketId);
 	}
-	
-	closeSocket(sockid);
+	free(socketId);
 	return 0;
 }
